eaac8720167c091b7302c86fcbf60861d9b5d4f1
Dana Ng <ngdana1@gmail.com>
2021-03-28 19:38:02 -0700

    Setup basic data

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+import {database as data} from './database';
 
 function App() {
   return (
@@ -6,6 +7,16 @@ function App() {
       <header className="App-header">
        30 Days of Chess ♟️
       </header>
+      <main className="days">
+        {data.map((content, index) => {
+          return (<div className="day">
+            <h2>{`Day ${index + 1}: ${content.title}`}</h2>
+            <p>
+              {content.description}
+            </p>
+          </div>)
+        })}
+      </main>
     </div>
   );
 }
