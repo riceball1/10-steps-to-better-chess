5acab6e08e3a38cf08a7a7f26f5b5fe3e1521b94
Dana Ng <ngdana1@gmail.com>
2021-04-03 17:49:38 -0700

    Created 30 day grid

--- src/App.js
+++ src/App.js
@@ -2,19 +2,37 @@ import './App.css';
 import {database as data} from './database';
 
 function App() {
+  const GRID_ROW_LENGTH = 5;
+  const GRID_COL_LENGTH = 6;
+ 
+  const grid = [];
+  for (let row = 0; row < GRID_ROW_LENGTH; row++) {
+    const currentRow = [];
+    for (let col = 0; col < GRID_COL_LENGTH; col++) {
+      const id = row + (col * GRID_ROW_LENGTH) + 1; 
+      if(id <=75) {
+        currentRow.push(<div className="box" id={id}>{id}</div>);
+      } else {
+        currentRow.push(<div className="box" id={id}></div>)
+      }
+    }
+    grid.push(currentRow);
+  }
+
   return (
     <div className="App">
       <header className="App-header">
        30 Days of Chess ♟️
       </header>
-      <main className="days">
-        {data.map((content, index) => {
-          return (<div className="day">
-            <h2>{`Day ${index + 1}: ${content.title}`}</h2>
-            <p>
-              {content.description}
-            </p>
-          </div>)
+      <main className="box-grid">
+      {grid.map((row, rowId) => {
+          return (
+            <div key={rowId}>
+              {row.map((node, nodeId) => {
+                return node;
+              })}
+            </div>
+          );
         })}
       </main>
     </div>
