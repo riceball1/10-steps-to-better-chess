528e2e556cd8cfbcc58881161802cd78021870b1
Dana Ng <ngdana1@gmail.com>
2021-04-14 21:32:21 -0700

    Updated route

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ import StepPage from "./pages/StepPage";
 import "./App.css";
 
 const routes = {
-  "/10-steps-to-better-chess": () => <Home />,
+  "/10-steps-to-better-chess/": () => <Home />,
   "/10-steps-to-better-chess/:value": ({ value }) => {
     const num = Number(value);
     return typeof num == "number" ? <StepPage value={num} /> : <NotFoundPage />;
