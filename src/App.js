a53a1f6d5fa4fd7066c5cc905a0fc0cb7fe5d6fd
Dana Ng <ngdana1@gmail.com>
2021-05-17 00:08:22 -0700

    Add cards and adjsut styling

--- src/App.js
+++ src/App.js
@@ -2,17 +2,10 @@
  * App - main file to start page
  */
 import ReactAnime from "react-animejs";
-import ChessPiece from "./assets/chesspiece.png";
 import "./App.css";
 const { Anime } = ReactAnime;
 
 function App() {
-  // var textWrapper = document.querySelector(".ml6 .letters");
-  // textWrapper.innerHTML = textWrapper.textContent.replace(
-  //   /\S/g,
-  //   "<span class='letter'>$&</span>"
-  // );
-
   const msg = "Learn ♟️ Chess";
 
   return (
@@ -25,10 +18,10 @@ function App() {
       initial={[
         {
           targets: ".letter",
-          translateY: ["1.3em", 0],
+          translateY: ["1.3rem", 0],
           translateZ: 0,
           duration: 1000,
-          delay: (el, i) => 50 * i
+          delay: (el, i) => 50 * i,
         },
         {
           targets: ".ml6",
@@ -42,12 +35,22 @@ function App() {
       <div className="container">
         <h1 className="m16">
           <span className="text-wrapper">
-            <span className="letters">{[...msg].map((letter, index) => {
-              return <span className="letter" key={letter + index + msg[index]}>{letter}</span>
-            })}</span>
+            <span className="letters">
+              {[...msg].map((letter, index) => {
+                return (
+                  <span className="letter" key={letter + index + msg[index]}>
+                    {letter}
+                  </span>
+                );
+              })}
+            </span>
           </span>
         </h1>
-        <img src={ChessPiece} alt="white queen chess piece" id="chesspiece" />
+        <div className="cardWrapper">
+          <div className="card" tabIndex="0">What is Chess?</div>
+          <div className="card" tabIndex="1">Chess Pieces</div>
+          <div className="card" tabIndex="2">How to Play</div>
+        </div>
       </div>
     </Anime>
   );
