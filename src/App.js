3e643489b3309142d250fba2a2848d0953bbe6a0
Dana Ng <ngdana1@gmail.com>
2021-05-16 23:45:20 -0700

    Update style

--- src/App.js
+++ src/App.js
@@ -2,31 +2,52 @@
  * App - main file to start page
  */
 import ReactAnime from "react-animejs";
+import ChessPiece from "./assets/chesspiece.png";
 import "./App.css";
 const { Anime } = ReactAnime;
 
 function App() {
+  // var textWrapper = document.querySelector(".ml6 .letters");
+  // textWrapper.innerHTML = textWrapper.textContent.replace(
+  //   /\S/g,
+  //   "<span class='letter'>$&</span>"
+  // );
+
+  const msg = "Learn ♟️ Chess";
+
   return (
     <Anime
+      animeConfig={{
+        delay: 100,
+        loop: true,
+        autoplay: true,
+      }}
       initial={[
         {
-          targets: ".box",
-          delay: 100,
-          direction: "alternate",
-          easing: "spring",
-          duration: 3000,
-          translateX: 50,
-          loop: true,
-          autoplay: true,
+          targets: ".letter",
+          translateY: ["1.3em", 0],
+          translateZ: 0,
+          duration: 1000,
+          delay: (el, i) => 50 * i
+        },
+        {
+          targets: ".ml6",
+          opacity: 0,
+          duration: 1000,
+          easing: "easeOutExpo",
+          delay: 1000,
         },
       ]}
     >
       <div className="container">
-        <div className="box">C</div>
-        <div className="box">H</div>
-        <div className="box">E</div>
-        <div className="box">S</div>
-        <div className="box">S</div>
+        <h1 className="m16">
+          <span className="text-wrapper">
+            <span className="letters">{[...msg].map((letter, index) => {
+              return <span className="letter" key={letter + index + msg[index]}>{letter}</span>
+            })}</span>
+          </span>
+        </h1>
+        <img src={ChessPiece} alt="white queen chess piece" id="chesspiece" />
       </div>
     </Anime>
   );
